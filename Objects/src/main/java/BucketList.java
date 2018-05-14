9e4fcec19c0d47190c7d36702b9cc184e4e5c8f5
Holburrito <ceflemons@gmail.com>
2018-05-13 18:01:46 -0600

    Build member and list

--- Objects/src/main/java/BucketList.java
+++ Objects/src/main/java/BucketList.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.List;
 
 public class BucketList {
+
+    private List<String> bucket;
+    private Member owner;
+
+    public BucketList(List<String> bucket, Member owner) {
+        this.bucket = bucket;
+        this.owner = owner;
+    }
+
+    public List<String> getBucket() {
+        return bucket;
+    }
+
+    public BucketList add(String listItem) {
+        bucket.add(listItem);
+        return this;
+    }
+
+    public Member getOwner() {
+        return owner;
+    }
 }
--- Objects/src/main/java/Member.java
+++ Objects/src/main/java/Member.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class Member {
+
+    private String email;
+    private String userName;
+    private BucketList bucketList;
+
+    public Member(String email, String userName) {
+        this.email = email;
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public BucketList getBucketList() {
+        return bucketList;
+    }
+
+    public void setBucketList(BucketList bucketList) {
+        this.bucketList = bucketList;
+    }
 }
